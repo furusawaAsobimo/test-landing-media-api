476df85e4c0fa789ca68dc3decb5b8ec0cec64ed
furusawaAsobimo <y_furusawa@asobimo.com>
2018-05-28 13:12:38 +0900

    setting ico bench api update all pages

--- public/ico_bench_api_all.php
+++ public/ico_bench_api_all.php
@@ -1,9 +1,11 @@
 <?php 
 // Composerオートローダを読み込む
 require_once '../vendor/autoload.php';
+// page指定
+$page = isset($_GET['page']) ? $_GET['page'] : 0;
 
 $api = new ICObenchAPI();
-$api->getICOs("all",["orderDesc"=>"rating","status"=>"active"]);
+$api->getICOs("all",["orderDesc"=>"rating","status"=>"active","page"=>$page]);
 
 echo $api->result;
 exit;
