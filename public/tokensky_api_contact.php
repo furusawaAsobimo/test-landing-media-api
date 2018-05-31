cf9b4f65868c23a74f847e04b7f4883ba4677c98
furusawaAsobimo <y_furusawa@asobimo.com>
2018-05-31 12:48:43 +0900

    setting tokensky contact api send judge add.

--- public/tokensky_api_contact.php
+++ public/tokensky_api_contact.php
@@ -27,27 +27,31 @@ try{
         throw new Exception('validate error');
     }
 
-    // 問い合わせ内容を格納
-    $body  = "tokenskyJPに、以下のお問い合わせがありました。\n\n";
-    $body .= "■お問い合わせ種別：{$inputData['contact_type']}\n";
-    $body .= "■出展社名（和名）：{$inputData['store_name_ja']}\n";
-    $body .= "■出展社名（カナ）：{$inputData['store_name_kana']}\n";
-    $body .= "■出展社名（英語）：{$inputData['store_name_en']}\n";
-    $body .= "■会社名：{$inputData['company']}\n";
-    $body .= "■会社名（カナ）：{$inputData['company_kana']}\n";
-    $body .= "■氏名：{$inputData['name']}\n";
-    $body .= "■氏名（カナ）：{$inputData['name_kana']}\n";
-    $body .= "■メールアドレス：{$inputData['email']}\n";
-    $body .= "■郵便番号：{$inputData['postal_code']}\n";
-    $body .= "■住所：{$inputData['address']}\n";
-    $body .= "■TEL：{$inputData['tel']}\n";
-    $body .= "■お問い合わせ内容：\n";
-    $body .= "-----------------------------\n";
-    $body .= $inputData['content']."\n";
-    $body .= "-----------------------------\n";
+    // メール送信する場合
+    if(isset($_POST['send_exec']) && !empty($_POST['send_exec'])){
 
-    // メール送信
-    $mail->sendMail('y_furusawa@asobimo.com', $inputData['email'], '[tokenskyJP]お問い合わせ', $body);
+        // 問い合わせ内容を格納
+        $body  = "tokenskyJPに、以下のお問い合わせがありました。\n\n";
+        $body .= "■お問い合わせ種別：{$inputData['contact_type']}\n";
+        $body .= "■出展社名（和名）：{$inputData['store_name_ja']}\n";
+        $body .= "■出展社名（カナ）：{$inputData['store_name_kana']}\n";
+        $body .= "■出展社名（英語）：{$inputData['store_name_en']}\n";
+        $body .= "■会社名：{$inputData['company']}\n";
+        $body .= "■会社名（カナ）：{$inputData['company_kana']}\n";
+        $body .= "■氏名：{$inputData['name']}\n";
+        $body .= "■氏名（カナ）：{$inputData['name_kana']}\n";
+        $body .= "■メールアドレス：{$inputData['email']}\n";
+        $body .= "■郵便番号：{$inputData['postal_code']}\n";
+        $body .= "■住所：{$inputData['address']}\n";
+        $body .= "■TEL：{$inputData['tel']}\n";
+        $body .= "■お問い合わせ内容：\n";
+        $body .= "-----------------------------\n";
+        $body .= $inputData['content']."\n";
+        $body .= "-----------------------------\n";
+
+        // メール送信
+        $mail->sendMail('y_furusawa@asobimo.com', $inputData['email'], '[tokenskyJP]お問い合わせ', $body);
+    }
 
 } catch (Exception $e) {
     $res['status'] = 'NG';
